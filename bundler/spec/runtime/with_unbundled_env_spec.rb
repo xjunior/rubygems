facc500416dcde69c96173e97f77d51630d36c03
David Rodríguez <deivid.rodriguez@riseup.net>
2021-09-23 20:16:21 +0200

    Allow casing differences for Windows

--- bundler/spec/runtime/with_unbundled_env_spec.rb
+++ bundler/spec/runtime/with_unbundled_env_spec.rb
@@ -66,9 +66,9 @@ RSpec.describe "Bundler.with_env helpers" do
       backup = ENV.to_hash.delete_if {|k, _v| k.start_with?(Bundler::EnvironmentPreserver::BUNDLER_PREFIX) }
       Bundler::EnvironmentPreserver.replace_env_with(backup)
 
-      original = ruby('puts ENV.to_a.map {|e| e.join("=") }.sort.join("\n")')
+      original = ruby('puts ENV.to_a.map {|k, v| [k.upcase, v].join("=") }.sort.join("\n")')
       create_file("source.rb", <<-RUBY)
-        puts Bundler.original_env.to_a.map {|e| e.join("=") }.sort.join("\n")
+        puts Bundler.original_env.to_a.map {|k, v| [k.upcase, v].join("=") }.sort.join("\n")
       RUBY
       bundle_exec_ruby bundled_app("source.rb")
       expect(out).to eq original
