2ea0e8ee9983e7ce2a863a076f0f8215b9156270
David Rodriguez <deivid.rodriguez@riseup.net>
2021-10-19 11:46:15 +0200

    Support using the `pathname` gem in the Gemfile

--- bundler/spec/runtime/setup_spec.rb
+++ bundler/spec/runtime/setup_spec.rb
@@ -1261,7 +1261,6 @@ end
         end << "bundler"
         exempts << "fiddle" if Gem.win_platform? && Gem.rubygems_version >= Gem::Version.new("2.7")
         exempts << "uri" if Gem.ruby_version >= Gem::Version.new("2.7")
-        exempts << "pathname" if Gem.ruby_version >= Gem::Version.new("3.0")
         exempts << "set" unless Gem.rubygems_version >= Gem::Version.new("3.2.6")
         exempts << "tsort" if Gem.ruby_version >= Gem::Version.new("3.0")
         exempts
